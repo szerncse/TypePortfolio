23e53a4992b5610f0615cff46b69c8128e61f328
정서진 <94szerncse@gmail.com>
2023-11-01 13:12:55 +0900

    202311 bar

--- component/skills.tsx
+++ component/skills.tsx
@@ -14,6 +14,7 @@ interface SkillData{
 
 export default function Skills(){
     const [itemList, setItemList] = useState<SkillItem[]>([]);
+    const [progressBar, setProgressBar] = useState<boolean>(false);
  
   
     useEffect(()=>{
@@ -22,12 +23,17 @@ export default function Skills(){
               const res = await fetch('/api/skill');
               const data = await res.json();
               setItemList(data.data);
+
+              setTimeout(()=>{
+                setProgressBar(true);
+              },500)
           }catch(error){
               console.log(error)
           } 
       }
       fetchData();
 },[])
+
 
     return(
         <>
@@ -37,7 +43,8 @@ export default function Skills(){
                 return(
                     <div className="w-full h-20" key={i}>
                         <div className="w-full h-5 bg-[#e0e0de] rounded-[50px]">
-                            <div className="h-full bg-sky-500 duration-1000 ease-out rounded-[50px] text-right relative mb-3 first:mt-10" style={{width: e.progressAfter+"%"}}>
+                            <div className="h-full bg-sky-500 duration-1000 ease-out rounded-[50px] text-right relative mb-3 first:mt-10" 
+                            style={{width: !progressBar ? e.progressBefore : e.progressAfter+"%"}}>
                                 <p className="absolute -top-6 md-top-7 left-1 text-sm md:text-lg">{e.title}</p>
                             </div>
                         </div>
